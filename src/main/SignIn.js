49c81f976fdad33c6ee13c8453b1bc2616952b81
Blessing-Tolani <ojoblessing999@gmail.com>
2022-02-06 22:37:28 +0100

    improvement: sign in form width

--- src/main/SignIn.js
+++ src/main/SignIn.js
@@ -13,7 +13,7 @@ const SignIn = () => {
     setChecked(checked ? false : true);
   };
   return (
-    <div className="border-2  border-lightGray rounded-md bg-gradient-to-tr from-darkGray to-lightGray w-52 mt-8 flex justify-center ">
+    <div className="border-2  border-lightGray rounded-md bg-gradient-to-tr from-darkGray to-lightGray w-60 mt-8 flex justify-center ">
       <div className="">
         <h2 className="my-3 text-gray-200 text">Sign in</h2>
         <form className="">
@@ -21,14 +21,14 @@ const SignIn = () => {
             <input
               type="email"
               placeholder="Email address"
-              className="w-44 h-8 bg-inputBgGray border border-inputBorderGray  text-white  placeholder:text-gray-300 placeholder:text-xxs placeholder:font-semibold flex items-center pb-1 pl-2 text-sm rounded"
+              className="w-44 h-8 bg-inputBgGray outline-none border border-inputBorderGray  text-white  placeholder:text-gray-300 placeholder:text-xxs placeholder:font-semibold flex items-center pb-1 pl-2 text-sm rounded"
             />
           </div>
           <div className="my-3 relative">
             <input
               type={passwordShown ? "text" : "password"}
               placeholder="Password"
-              className="text-sm text-white shadow-md bg-inputBgGray border border-inputBorderGray placeholder:text-gray-300 placeholder:text-xxs placeholder:font-semibold  h-8 w-44 pl-2 pb-1 rounded"
+              className="text-sm text-white outline-none shadow-md bg-inputBgGray border border-inputBorderGray placeholder:text-gray-300 placeholder:text-xxs placeholder:font-semibold  h-8 w-44 pl-2 pb-1 rounded"
             />
             <AiOutlineEye
               className="absolute text-gray-300 right-3 top-2 cursor-pointer"
@@ -38,7 +38,7 @@ const SignIn = () => {
           <div className={checked ? "checked" : "unchecked"}>
             <input
               type="checkbox"
-              className="mr-1 cursor-pointer"
+              className="mr-1 cursor-pointer outline-none"
               onClick={toggleCheckBox}
             />
             <label className="text-xxs  text-gray-300 font-light ">
@@ -48,7 +48,7 @@ const SignIn = () => {
           <div>
             <button
               type="submit"
-              className="w-44 h-8 mt-3 pt-2 pb-3 bg-white text-center rounded text-darkGray text-xxs font-semibold"
+              className="w-44 h-8 mt-3 outline-none pt-2 pb-3 bg-white text-center rounded text-darkGray text-xxs font-semibold"
             >
               SIGN IN
             </button>

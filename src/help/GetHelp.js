8c9798704c04c206ceead94ca65f9bdf4654ff91
Blessing-Tolani <ojoblessing999@gmail.com>
2022-02-06 23:01:44 +0100

    improvement: width of the sign in form

--- src/help/GetHelp.js
+++ src/help/GetHelp.js
@@ -1,7 +1,7 @@
 const GetHelp = () => {
   return (
     <div>
-      <p className="w-40  font-light text-xs pt-20 text-gray-200 ">
+      <p className="w-48  font-light text-sm pt-20 text-gray-200 ">
         If you find difficulty accessing your account, Get help{" "}
         <a href="https://risevest.com/" className="underline">
           here
--- src/main/SignIn.js
+++ src/main/SignIn.js
@@ -14,22 +14,22 @@ const SignIn = () => {
   };
   return (
     <div className="w-full">
-      <div className="border-2  border-lightGray rounded-md bg-gradient-to-tr from-darkGray to-lightGray w-56  mt-8 flex justify-center ">
+      <div className="border-2  border-lightGray rounded-md bg-gradient-to-tr from-darkGray to-lightGray w-72  mt-8 flex justify-center ">
         <div className="">
-          <h2 className="my-3 text-gray-200 text">Sign in</h2>
+          <h2 className="my-5 text-gray-200 text text-lg">Sign in</h2>
           <form className="">
             <div>
               <input
                 type="email"
                 placeholder="Email address"
-                className="w-44 h-8 bg-inputBgGray outline-none border border-inputBorderGray  text-white  placeholder:text-gray-300 placeholder:text-xs placeholder:font-semibold flex items-center pb-1 pl-2 text-sm rounded"
+                className="w-56 h-8 bg-inputBgGray outline-none border border-inputBorderGray  text-white  placeholder:text-gray-300 placeholder:text-xs placeholder:font-semibold flex items-center pb-1 pl-2 text-sm rounded"
               />
             </div>
-            <div className="my-3 relative">
+            <div className="my-4 relative">
               <input
                 type={passwordShown ? "text" : "password"}
                 placeholder="Password"
-                className="text-sm text-white outline-none shadow-md bg-inputBgGray border border-inputBorderGray placeholder:text-gray-300 placeholder:text-xs placeholder:font-semibold  h-8 w-44 pl-2 pb-1 rounded"
+                className="text-sm text-white outline-none shadow-md bg-inputBgGray border border-inputBorderGray placeholder:text-gray-300 placeholder:text-xs placeholder:font-semibold  h-8 w-56 pl-2 pb-1 rounded"
               />
               <AiOutlineEye
                 className="absolute text-gray-300 right-3 top-2 cursor-pointer"
@@ -49,12 +49,12 @@ const SignIn = () => {
             <div>
               <button
                 type="submit"
-                className="w-44 h-8 mt-3 outline-none pt-2 pb-3 bg-white text-center rounded text-darkGray text-xs font-semibold"
+                className="w-56 h-8 mt-3 outline-none pt-2 pb-3 bg-white text-center rounded text-darkGray text-xs font-semibold"
               >
                 SIGN IN
               </button>
             </div>
-            <p className="py-4 font-light text-gray-400 text-xs">
+            <p className="pt-4 pb-6 font-light text-gray-400 text-xs">
               I forgot my password
             </p>
           </form>
